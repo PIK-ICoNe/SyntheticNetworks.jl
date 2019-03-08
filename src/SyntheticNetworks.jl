c1e06fbf1aabe3be023bd2e4bfb7c6c4173527b8
oskarddfa <oskar.pfeffer@gmail.com>
2019-03-08 18:12:11 +0100

    First working version of Paul's first paper. Uncommented and may have bugs

--- src/SyntheticNetworks.jl
+++ src/SyntheticNetworks.jl
@@ -5,9 +5,8 @@ module SyntheticNetworks
 using Random
 using Parameters
 using LightGraphs
-using MetaGraphs
-using SpatialIndexing
-
+# using SpatialIndexing
+using EmbeddedGraphs
 """
     greet()
 
@@ -35,7 +34,7 @@ export greet
     SyntheticNetwork
 """
 abstract type SyntheticNetwork end
-export SyntheticNetwork
+export SyntheticNetwork, Grid_Growth_Parameters, initialise, generate_graph, f
 
 """
     RandomPowerGrid(num_layers, n, n0, p, q, r, s, u, sampling, α, β, γ, debug)
@@ -76,24 +75,103 @@ export SyntheticNetwork
     cumrtree::AbstractArray
 end
 export RandomPowerGrid
-function _rand_uniform()
-    2*(0.5-rand())
-end
+
+
 
 struct Grid_Growth_Parameters
+    n::Int
     n0::Int
+    p::Float32
+    q::Float32
+    r::Float32
+    s::Float32
+    u::Float32
 end
 
-function initialise(grid_pars)
-    n0 = grid_pars.n0
-    positions = [[_rand_uniform(), _rand_uniform()] for i=1:n0 ]
-    graph = EG.EmbeddedGraph(SimpleGraph(n0), positions, EG.distance)
-    mst_graph = EG.EmbeddedGraph(CompleteGraph(n0), positions, EG.distance)
+Grid_Growth_Parameters(n, n0, args...) = Grid_Growth_Parameters(n0, zeros(5)...)
 
-    for i in kruskal_mst(mst_graph)
+function generate_graph(gp)
+    n = gp.n
+    n0 = gp.n0
+    p = gp.p
+    q = gp.q
+    r = gp.r
+    s = gp.s
+    u = gp.u
+    g = initialise(n0, p, q, r, s, u)
+    grow!(g, n, n0, p, q, r, s, u)
+    g
+end
+
+function grow!(g::EmbeddedGraph, n::Int, n0::Int, p, q, r, s, u; vertex_density_prob=_rand_uniform)
+    for dummy in n0+1:n
+        if rand() >= s
+            # STEP G1
+            pos = [vertex_density_prob(), vertex_density_prob()]
+            add_vertex!(g, pos)
+
+            # STEP G2
+            d_spatial_i = map(i -> distance(g.vertexpos[nv(g)], g.vertexpos[i]), 1:nv(g))
+            d_spatial_i[nv(g)] = 100 #Inf
+            min_dist_vertex = argmin(d_spatial_i)
+            add_edge!(g, min_dist_vertex, nv(g))
+
+            # STEP G3
+            if rand() <= p
+                l_edge = _G34(g, nv(g), d_spatial_i, r)
+                add_edge!(g, l_edge, nv(g))
+            end
+            # STEP G4
+            if rand() <= q
+                i = rand(1:nv(g))
+                d_spatial_i = map(j -> distance(g.vertexpos[i], g.vertexpos[j]), 1:nv(g))
+                l_edge = _G34(g, nv(g), d_spatial_i, r)
+                add_edge!(g, l_edge, i)
+            end
+
+        else
+            # STEP G5
+            edge = rand(edges(g))
+            newpos = (g.vertexpos[src(edge)] + g.vertexpos[dst(edge)]) / 2.
+            add_vertex!(g, newpos)
+            add_edge!(g, src(edge), nv(g))
+            add_edge!(g, dst(edge), nv(g))
+            rem_edge!(g, edge)
+        end
+    end
+end
+
+function initialise(n0::Int, p, q, r, s, u; vertex_density_prob=_rand_uniform)
+    # STEP I1
+    positions = [[vertex_density_prob(), vertex_density_prob()] for i=1:n0 ]
+    graph = EmbeddedGraph(SimpleGraph(n0), positions)
+    # STEP I2
+    mst_graph = EmbeddedGraph(CompleteGraph(n0), positions)
+    edges = prim_mst(mst_graph, weights(mst_graph, dense=true))
+    for i in edges
         add_edge!(graph,i)
     end
+    # STEP I3
+    m = Int(round(n0*(1-s)*(p+q), RoundDown))
+    _I3!(graph, r, m)
     graph
+end
+
+function _I3!(g::EmbeddedGraph, r::Real, m::Int)
+    for dummy in 1:m
+        spatial = weights(g, dense=true)
+        A = floyd_warshall_shortest_paths(g, weights(g)).dists
+        A = ((A .+ spatial) .^ r) ./ spatial
+        map(i -> A[i,i] = 0, 1:size(A)[1])
+        add_edge!(g,Tuple(argmax(A))...)
+    end
+end
+
+function _G34(g::EmbeddedGraph, i::Int, d_spatial_i, r)
+    V = dijkstra_shortest_paths(g, i).dists
+    V = ((V .+ d_spatial_i) .^ r) ./ d_spatial_i
+    V[i] = 0
+    argmax(V)
 end
 
 function _rand_uniform()
