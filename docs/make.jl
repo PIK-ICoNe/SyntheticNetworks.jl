7e7b808b60bf34347c4b7dc782208d3c811f0c45
Paul Schultz <pschultz@pik-potsdam.de>
2020-09-03 12:29:55 +0200

    docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,19 +1,15 @@
-dir = @__DIR__
-push!(LOAD_PATH,"$dir/../src/")
-
 using Documenter
 using SyntheticNetworks
 
 makedocs(
     sitename = "SyntheticNetworks",
     format = Documenter.HTML(),
-    modules = [SyntheticNetworks])
+    modules = [SyntheticNetworks],
+)
 
 
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
 
-deploydocs(
-    repo = "github.com/luap-pik/SyntheticNetworks.git"
-)
+deploydocs(repo = "github.com/luap-pik/SyntheticNetworks.git")
